0d041f7a6ad2ad76d0744faff98beecce44b2cb0
Chris Crowley <cfcrowely42@gmail.com>
2021-08-30 09:44:31 -0400

    fixed errors and added get by nickname method in repo

--- Pokemon_Repository/PokemonRepository.cs
+++ Pokemon_Repository/PokemonRepository.cs
@@ -2,20 +2,24 @@
 
 namespace Pokemon_Repository
 {
-    private class PokemonRepository
+    public class PokemonRepository
     {
         List<Pokemon> _pokemonTeam = new List<Pokemon>();
-    }
 
         //add to list (limit pokemon to 6)
-        public AddPokemonToTeam(Pokemon pokemon)
+        public void AddPokemonToTeam(Pokemon pokemon)
         {
-            _pokemonTeam.Add(new Pokemon);
+            _pokemonTeam.Add(pokemon);
         }
         //get list
         public List<Pokemon> GetPokemonTeam()
         {
-
+            List<Pokemon> allPokes = new List<Pokemon>();
+            foreach(Pokemon p in _pokemonTeam)
+            {
+                allPokes.Add(p);
+            }
+            return allPokes;
         }
         //get one pokemon
         public Pokemon GetPokemonByTeamPosition(int teamPosition)
@@ -23,36 +27,56 @@ namespace Pokemon_Repository
             Pokemon pokemon = _pokemonTeam[teamPosition - 1];
             return pokemon;
         }
+        public Pokemon GetPokemonByNickName(string nickName)
+        {
+            foreach(Pokemon p in _pokemonTeam)
+            {
+                if (p.PokemonNickName == nickName)
+                {
+                    return p;
+                }
+            }
+            return null;
+        }
         //update pokemon
         public void UpdatePokemonByTeamPosition(int teamPosition, Pokemon updates)
         {
             Pokemon pokemon = _pokemonTeam[teamPosition - 1];
             pokemon.PokemonSpeciesName = updates.PokemonSpeciesName;
             pokemon.PokemonNickName = updates.PokemonNickName;
-            pokemon.level = updates.Level
-                pokemon.PokemonType = updates.PokemonType;
+            pokemon.Level = updates.Level;
+            pokemon.PokemonType = updates.PokemonType;
             pokemon.SecondaryType = updates.SecondaryType;
-            pokemon.MoveOne = updates.MoveOne
-                pokemon.Movetwo = updates.MoveTwo;
+            pokemon.MoveOne = updates.MoveOne;
+            pokemon.MoveTwo = updates.MoveTwo;
             pokemon.MoveThree = updates.MoveThree;
-            pokemon.MoveFour = updates.MoveFour
-            }
+            pokemon.MoveFour = updates.MoveFour;
+        }
 
         public void UpdatePokemonByNickName(string nickName, Pokemon newPokemon)
         {
-            foreach (Pokemon p in pokemonList)
-            {
-                if (nickName == p.PokemonNickName)
+            Pokemon oldPoke = GetPokemonByNickName(nickName);
+            
+                if (nickName != null)
                 {
-                    p = new Pokemon;
+                oldPoke.PokemonSpeciesName = newPokemon.PokemonSpeciesName;
+                oldPoke.PokemonNickName = newPokemon.PokemonNickName;
+                oldPoke.Level = newPokemon.Level;
+                oldPoke.PokemonType = newPokemon.PokemonType;
+                oldPoke.SecondaryType = newPokemon.SecondaryType;
+                oldPoke.MoveOne = newPokemon.MoveOne;
+                oldPoke.MoveTwo = newPokemon.MoveTwo;
+                oldPoke.MoveThree = newPokemon.MoveThree;
+                oldPoke.MoveFour = newPokemon.MoveFour;
                 }
-            }
+            
         }
 
         //remove pokemon
         public void RemovePokemonFromTeam(int teamPosition)
         {
             Pokemon pokemon = _pokemonTeam[teamPosition - 1];
-            _pokemonTeam.Dequeue(pokemon);
+            _pokemonTeam.Remove(pokemon);
         }
+    }
 }
